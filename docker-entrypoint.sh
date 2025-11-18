70983c9c7491d2e98f4ca1577d918e303f4d0bfc
Timo <email@timobraun.de>
2025-11-18 23:25:53 +0100

    Handle special PayPal pool IDs and optional donation text

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,8 +10,9 @@ set -e
 : "${FPP_PLAYLIST_IDLE:=background}"
 : "${FPP_POLL_INTERVAL_MS:=30000}"
 : "${CLIENT_STATUS_POLL_MS:=10000}"
-: "${DONATION_PAYPAL:=}"
-: "${DONATION_TEXT:=Vielen Dank für deine Unterstützung!}"
+: "${DONATION_POOL_ID:=}"
+: "${DONATION_CAMPAIGN_NAME:=}"
+: "${DONATION_TEXT:=}"
 : "${PREVIEW_MODE:=false}"
 : "${ACCESS_CODE:=}"
 
@@ -20,16 +21,19 @@ python - <<'PY'
 import json
 import os
 
+donation_text_env = os.getenv("DONATION_TEXT")
+
 config = {
     "siteName": os.getenv("SITE_NAME", "Brauns Lichtershow"),
     "siteSubtitle": os.getenv(
         "SITE_SUBTITLE", "Fernsteuerung für den Falcon Player"
     ),
     "statusPollMs": int(os.getenv("CLIENT_STATUS_POLL_MS", "10000")),
-    "donationPaypal": os.getenv("DONATION_PAYPAL", ""),
-    "donationText": os.getenv(
-        "DONATION_TEXT", "Vielen Dank für deine Unterstützung!"
-    ),
+    "donationPoolId": os.getenv("DONATION_POOL_ID", ""),
+    "donationCampaignName": os.getenv("DONATION_CAMPAIGN_NAME", ""),
+    "donationText": "Vielen Dank für deine Unterstützung!"
+    if donation_text_env is None
+    else donation_text_env,
     "previewMode": os.getenv("PREVIEW_MODE", "false").lower()
     in ["true", "1", "yes", "on"],
     "accessCode": os.getenv("ACCESS_CODE", ""),
